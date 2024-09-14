39cdaca173496c20a8da638a22e6bd1337ccf4d8
Liamb55 <165090631+Liamb55@users.noreply.github.com>
2024-09-14 19:23:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,3 @@ set -o errexit
 
 pip freeze > requirements.txt
 pip install -r requirements.txt
-
-python manage.py makemigrations
-python manage.py collectstatic --no-input
-python manage.py migrate
